dcbfb5574589a71e3126d4905ab425607706e980
Buiducan <buiducanclct2002@gmail.com>
2024-03-25 16:59:47 +0700

    handle AddBtn

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import { AiOutlineDelete } from "react-icons/ai";
+import { BsCheckLg } from "react-icons/bs";
 
 function App() {
     const [isCompleteScreen, setIsCompleteScreen] = useState(false);
+    const [allTodos, setTodos] = useState([]);
+    const [newTitle, setNewTitle] = useState("");
+    const [newDescription, setNewDescription] = useState("");
+    const handleAddtodo = () => {
+        let newTodoItem = {
+            title: newTitle,
+            description: newDescription,
+        };
+        let updatedTodoArr = [...allTodos];
+        updatedTodoArr.push(newTodoItem);
+        setTodos(updatedTodoArr);
+        localStorage.setItem("todolist", JSON.stringify(updatedTodoArr));
+    };
+    useEffect(() => {
+        let saveTodo = JSON.parse(localStorage.getItem("todolist"));
+        if (saveTodo) {
+            setTodos(saveTodo);
+        }
+    }, []);
     return (
         <div className="App">
             <h1>My Todos</h1>
@@ -13,6 +34,8 @@ function App() {
                         <input
                             type="text"
                             placeholder="What's the task title?"
+                            value={newTitle}
+                            onChange={(e) => setNewTitle(e.target.value)}
                         />
                     </div>
                     <div className="todo-input-item">
@@ -20,10 +43,16 @@ function App() {
                         <input
                             type="text"
                             placeholder="What's the task title?"
+                            value={newDescription}
+                            onChange={(e) => setNewDescription(e.target.value)}
                         />
                     </div>
                     <div className="todo-input-item">
-                        <button type="button" className="primaryBtn">
+                        <button
+                            type="button"
+                            className="primaryBtn"
+                            onClick={handleAddtodo}
+                        >
                             Add
                         </button>
                     </div>
@@ -47,10 +76,26 @@ function App() {
                     </button>
                 </div>
                 <div className="todo-list">
-                    <div className="todo-list-item">
-                        <h3>Task 1</h3>
-                        <p>Description</p>
-                    </div>
+                    {allTodos.map((item, index) => {
+                        return (
+                            <div className="todo-list-item" key={index}>
+                                <div>
+                                    <h3>{item.title}</h3>
+                                    <p>{item.description}</p>
+                                </div>
+                                <div>
+                                    <AiOutlineDelete
+                                        className="icon"
+                                        title="Delete"
+                                    />
+                                    <BsCheckLg
+                                        className="check-icon"
+                                        title="Complete"
+                                    />
+                                </div>
+                            </div>
+                        );
+                    })}
                 </div>
             </div>
         </div>
